9d260907cff6de6ea532b760dc9d4da68e82684a
mcagri <mcagridurgut@gmail.com>
2021-04-30 17:26:15 +0300

    startup

--- backend/Startup.cs
+++ backend/Startup.cs
@@ -11,14 +11,13 @@ using backend.Services.JoinRequestServices;
 using backend.Services.ProjectGroupServices;
 using backend.Services.MergeRequestServices;
 using backend.Services.SubmissionServices;
-using backend.Services.PeerGradeServices;
-using backend.Services.ProjectGradeServices;
 // using backend.Services.Email;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -28,8 +27,10 @@ using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.Text.Json.Serialization;
-using backend.Services.CourseServices;
 using backend.Services.SectionServices;
+using backend.Services.CourseServices;
+using backend.Services.PeerGradeServices;
+using backend.Services.ProjectGradeServices;
 
 namespace backend
 {
@@ -45,8 +46,19 @@ namespace backend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // var emailConfig = Configuration.GetSection("EmailConfiguration").Get<EmailConfiguration>();
+            // services.AddSingleton(emailConfig);
             services.AddDbContext<DataContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
+            services.AddCors(
+                options => options.AddDefaultPolicy(
+                    builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()
+                )
+            );
+            // .AddJsonOptions(options =>
+            // {
+            //     options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+            // });
 
             services.AddSwaggerGen(c =>
             {
@@ -75,10 +87,6 @@ namespace backend
                         },
                         new List<string>()
                     } });
-                services.AddCors(
-                    options => options.AddDefaultPolicy(
-                        builder => builder.AllowAnyOrigin()
-                    ));
             });
 
             services.AddAutoMapper(typeof(Startup));
@@ -117,18 +125,14 @@ namespace backend
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BilHub v1"));
             }
 
-
-
             //app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseCors(c => c.AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin());
 
             app.UseAuthentication();
 
-            app.UseCors(builder => builder.AllowAnyOrigin());
-
             app.UseAuthorization();
-
 
             app.UseEndpoints(endpoints =>
             {
